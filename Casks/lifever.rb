5b6cc7e782e0ff16d4e76166fe9d708efc204e57
Bartosz Król <m@badosz.com>
2026-07-25 23:08:41 +0200

    chore(homebrew): update Lifever to 0.1.0

---
+++ Casks/lifever.rb
@@ -0,0 +1,27 @@
+cask "lifever" do
+  version "0.1.0"
+  sha256 "5e2ee3452c99acd58d744ea8e34c7a7e6b186f7cf88326a4deebe9e5109e2190"
+
+  url "https://github.com/badosz0/lifever/releases/download/v#{version}/Lifever-#{version}-macOS-universal.dmg",
+      verified: "github.com/badosz0/lifever/"
+  name "Lifever"
+  desc "Calm, modular home for the everyday parts of life"
+  homepage "https://github.com/badosz0/lifever"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Lifever.app"
+
+  zap trash: [
+    "~/Library/Application Support/app.lifever.desktop",
+    "~/Library/Caches/app.lifever.desktop",
+    "~/Library/Preferences/app.lifever.desktop.plist",
+    "~/Library/Saved Application State/app.lifever.desktop.savedState",
+    "~/Library/WebKit/app.lifever.desktop",
+  ]
+end
